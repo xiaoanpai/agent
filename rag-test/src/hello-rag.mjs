a56ee02fd1b3b444095d07cb84c0b6e2291777ab
xiaoanpai <d177686@163.com>
2026-09-18 22:51:06 +0800

    init

---
+++ rag-test/src/hello-rag.mjs
@@ -0,0 +1,10 @@
+import "dotenv/config"
+import {ChatOpenAI,OpenAIEmbeddings} from "@langchain/openai"
+import {Document} from "@langchain/core/documents"
+import {MemoryVectorStore} from "@langchain/classic/vectorstores/memory"
+
+const model = new ChatOpenAI({
+    temperature: 0,
+    model: process.env.MODEL_NAME,
+    
+})
